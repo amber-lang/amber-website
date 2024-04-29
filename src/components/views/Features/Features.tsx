0d4a11525a57f8d8bd111825d3bccbe7254f8689
Phoenix Himself <pkaras.it@gmail.com>
2024-04-30 00:32:33 +0200

    fix: big resolutions

--- src/components/views/Features/Features.tsx
+++ src/components/views/Features/Features.tsx
@@ -9,21 +9,21 @@ export default function Features() {
                 <Image src="/icons/syntax.svg" alt="types" width={32} height={32} />
                 <div className={styles.title}>Modern Syntax</div>
                 <p className={styles.paragraph}>
-                    The evolution of programming language syntax has significantly enhanced the ability of developers to create robust solutions efficiently. Modern programming languages, with their intuitive and concise syntax, empower users to write clearer, more maintainable code, facilitating quicker debugging and easier collaboration.
+                    You will find many of the language features familiar, allowing you to get up and running much faster than if you were learning Bash from scratch.
                 </p>
             </Card>
             <Card style="dark">
                 <Image src="/icons/runtime.svg" alt="types" width={32} height={32} />
                 <div className={styles.title}>Runtime Safety</div>
                 <p className={styles.paragraph}>
-                    The evolution of programming language syntax has significantly enhanced the ability of developers to create robust solutions efficiently. Modern programming languages, with their intuitive and concise syntax, empower users to write clearer, more maintainable code, facilitating quicker debugging and easier collaboration.
+                    It’s one of the key components missing from regular shell scripts. It can help you catch many bugs at compile time.
                 </p>
             </Card>
             <Card style="dark">
                 <Image src="/icons/types.svg" alt="types" width={32} height={32} />
                 <div className={styles.title}>Type Safety</div>
                 <p className={styles.paragraph}>
-                    The evolution of programming language syntax has significantly enhanced the ability of developers to create robust solutions efficiently. Modern programming languages, with their intuitive and concise syntax, empower users to write clearer, more maintainable code, facilitating quicker debugging and easier collaboration.
+                    Amber ensures that you handle everything that could fail. Each Bash command and function that could fail must be handled in some way.
                 </p>
             </Card>
         </div>
