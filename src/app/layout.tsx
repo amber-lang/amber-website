441f6a24b14cf7b498a45f273f649489f42e5a2b
Phoenix Himself <pkaras.it@gmail.com>
2024-05-05 17:36:23 +0200

    fix: link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
       type: "website",
       images: [
           {
-              url: 'https://amber-lang.com/og.png'
+              url: 'https://amber-lang.com/og.jpeg'
           }
       ]
   }
