0763ece45da712031ead830113653c189e737cd0
Phoenix Himself <pkaras.it@gmail.com>
2024-05-10 16:42:50 +0200

    fix: snippet

--- src/components/atoms/CopyInstaller/CopyInstaller.tsx
+++ src/components/atoms/CopyInstaller/CopyInstaller.tsx
@@ -3,49 +3,37 @@
 import { useEffect, useState } from "react";
 import styles from "./CopyInstaller.module.css";
 
-type Contents = {
-    name: string;
-    snippet: string;
-}
+const SNIPPET = "sudo curl -s \"https://raw.githubusercontent.com/Ph0enixKM/AmberNative/master/setup/install.sh\" | $(echo /bin/bash)"
 
-function getContents(): Contents | null {
+function getOSName(): string | null {
     const userAgent = (typeof window !== "undefined") ? window.navigator.userAgent : "";
     switch (true) {
         case /Mac/img.test(userAgent):
-            return {
-                name: "Download for ",
-                snippet: "sudo ruby -e \"require 'open-uri'; puts open('https://raw.githubusercontent.com/Ph0enixKM/AmberNative/master/setup/install.sh').read\" | $(echo $SHELL)'`"
-            };
+            return "Download for ";
         case /Linux/img.test(userAgent):
-            return {
-                name: "Download for Linux",
-                snippet: "sudo curl \"https://raw.githubusercontent.com/Ph0enixKM/AmberNative/master/setup/install.sh\" | $(echo $SHELL)"
-            };
+            return "Download for Linux";
         case /Windows/.test(userAgent):
-            return {
-                name: "Download for WSL",
-                snippet: "sudo curl \"https://raw.githubusercontent.com/Ph0enixKM/AmberNative/master/setup/install.sh\" | $(echo $SHELL)"
-            };
+            return "Download for WSL";
         default:
             return null;
     }
 }
 
 export default function CopyInstaller() {
-    const [contents, setContents] = useState<Contents>();
+    const [osName, setOsName] = useState<string>();
     const [copied, setCopied] = useState<string>();
 
     useEffect(() => {
-        const contents = getContents();
-        if (contents) {
-            setContents(contents);
+        const osName = getOSName();
+        if (osName) {
+            setOsName(osName);
         }
     }, []);
 
     const onClick = () => {
         if (copied) return;
         setCopied(styles.copied);
-        navigator.clipboard.writeText(contents?.snippet ?? '');
+        navigator.clipboard.writeText(SNIPPET);
 
         fetch("/api/visit?name=download", {
             cache: 'no-store'
@@ -56,15 +44,15 @@ export default function CopyInstaller() {
         }, 1000);
     }
 
-    if (contents) {
+    if (osName) {
         return (
             <div className={styles.container}>
                 <div className={styles.name}>
-                    {contents.name}
+                    {osName}
                 </div>
                 <div className={[styles.copy, copied].join(' ')} onClick={onClick} />
                 <div className={styles.code}>
-                    {contents.snippet}
+                    {SNIPPET}
                 </div>
             </div>
         );
